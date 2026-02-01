4c481c374f38ef9f0366a637b5159949fb5790ee
manoj kumar <manojthakur614@gmail.com>
2026-02-01 14:47:44 +0530

    ina

--- ebooking/src/components/Booking.jsx
+++ ebooking/src/components/Booking.jsx
@@ -1,44 +1,44 @@
-// import { useState } from "react";
+import { useState } from "react";
 
 
-// export default function Booking({ event }) {
-// const [date] = useState(new Date().toDateString());
-// const slots = ["11:30 AM", "12:00 PM", "06:00 PM"];
+export default function Booking({ event }) {
+const [date] = useState(new Date().toDateString());
+const slots = ["11:30 AM", "12:00 PM", "06:00 PM"];
 
 
-// const book = (time) => {
-// const bookings = JSON.parse(localStorage.getItem("bookings")) || [];
-// bookings.push({ ...event, date, time });
-// localStorage.setItem("bookings", JSON.stringify(bookings));
-// alert("Booking confirmed");
-// };
+const book = (time) => {
+const bookings = JSON.parse(localStorage.getItem("bookings")) || [];
+bookings.push({ ...event, date, time });
+localStorage.setItem("bookings", JSON.stringify(bookings));
+alert("Booking confirmed");
+};
 
 
-// return (
-// <div>
-// <p>Today</p>
-// <p>Morning</p>
-// {slots.map(s => (
-// <button key={s} onClick={() => book(s)}>Book FREE Event</button>
-// ))}
-// </div>
-// );
-// }
+return (
+<div>
+<p>Today</p>
+<p>Morning</p>
+{slots.map(s => (
+<button key={s} onClick={() => book(s)}>Book FREE Event</button>
+))}
+</div>
+);
+}
 
-export default function Booking() {
-    return (
-      <div>
-        <p>Today</p>
+// export default function Booking() {
+//     return (
+//       <div>
+//         <p>Today</p>
   
-        <p>Morning</p>
-        <button>Book FREE Event</button>
+//         <p>Morning</p>
+//         <button>Book FREE Event</button>
   
-        <p>Afternoon</p>
-        <button>Book FREE Event</button>
+//         <p>Afternoon</p>
+//         <button>Book FREE Event</button>
   
-        <p>Evening</p>
-        <button>Book FREE Event</button>
-      </div>
-    );
-  }
+//         <p>Evening</p>
+//         <button>Book FREE Event</button>
+//       </div>
+//     );
+//   }
   
