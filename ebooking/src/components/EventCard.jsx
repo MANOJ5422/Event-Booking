fa5b519f0358a2a8285b7ca50820a590e74058d5
manoj kumar <manojthakur614@gmail.com>
2026-02-01 14:04:29 +0530

    eba

--- ebooking/src/components/EventCard.jsx
+++ ebooking/src/components/EventCard.jsx
@@ -1,42 +1,42 @@
-import { useState } from "react";
-import Booking from "./Booking";
-
-export default function EventCard({ event }) {
-  const [showBooking, setShowBooking] = useState(false);
-
-  return (
-    <div
-      className="event-card"
-      onClick={() => setShowBooking(true)}
-    >
-      <h3>{event.eventName}</h3>
-      <p>{event.address}</p>
-      <p>{event.city}, {event.state}</p>
-
-      {showBooking && <Booking event={event} />}
-    </div>
-  );
-}
-
-
-
-
-
-
-
-
-
+// import { useState } from "react";
 // import Booking from "./Booking";
 
-
 // export default function EventCard({ event }) {
-// return (
-// <div className="event-card">
-// <h3>{event.eventName}</h3>
-// <p>{event.address}</p>
-// <p>{event.city}, {event.state}</p>
-// <p>⭐ {event.rating}</p>
-// <Booking event={event} />
-// </div>
-// );
+//   const [showBooking, setShowBooking] = useState(false);
+
+//   return (
+//     <div
+//       className="event-card"
+//       onClick={() => setShowBooking(true)}
+//     >
+//       <h3>{event.eventName}</h3>
+//       <p>{event.address}</p>
+//       <p>{event.city}, {event.state}</p>
+
+//       {showBooking && <Booking event={event} />}
+//     </div>
+//   );
 // }
+
+
+
+
+
+
+
+
+
+import Booking from "./Booking";
+
+
+export default function EventCard({ event }) {
+return (
+<div className="event-card">
+<h3>{event.eventName}</h3>
+<p>{event.address}</p>
+<p>{event.city}, {event.state}</p>
+<p>⭐ {event.rating}</p>
+<Booking event={event} />
+</div>
+);
+}
